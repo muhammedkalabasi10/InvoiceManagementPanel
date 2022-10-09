e243c3a66f5c8d4bef4fee3587500bc94d9c51a6
muhammed kalabaşı <muhammedkalabasi@marun.edu.tr>
2022-10-09 23:55:32 +0300

    added api link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,23 @@
-/* import React from "react";
-//import { useLocation } from "react-router-dom";
-import styles from "./Footer.module.css";
-//import FabButton from '../Fab/Fab'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import styles from './Footer.module.css'
+import FabButton from '../Fab/Fab'
 
 const Footer = () => {
-  // const location = useLocation();
-  //const [user, setUser ] = useState(JSON.parse(localStorage.getItem('profile')))
+    const location = useLocation()
+    const [user, setUser ] = useState(JSON.parse(localStorage.getItem('profile')))
 
-  /* useEffect(() => {
+    useEffect(() => {
         setUser(JSON.parse(localStorage.getItem('profile')))
     }, [location])
-*/
-/* return (
-    <footer>
-      <div className={styles.footerText}>
-        <span>AKINCILAR</span>
-      </div>
-    </footer>
-  );
-};
 
-export default Footer;
-
-/* {user && (
+    return (
+        <footer>
+            {user && (
             <FabButton />
-            )}*/
+            )}
+        </footer>
+    )
+}
+
+export default Footer
