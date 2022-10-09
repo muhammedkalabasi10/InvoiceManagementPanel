2903ba53fef0dc67683736a0165a676f400ab0c5
muhammed kalabaşı <muhammedkalabasi@marun.edu.tr>
2022-10-10 00:35:09 +0300

    added api link

--- src/components/Fab/Fab.js
+++ src/components/Fab/Fab.js
@@ -6,17 +6,21 @@ import AddIcon from "@material-ui/icons/Add";
 import CreateIcon from "@material-ui/icons/Create";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
 import AddClient from "../Invoice/AddClient";
+import AddProduct from '../Invoice/AddProduct';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 const FabButton = () => {
   const location = useLocation();
   const mainButtonStyles = { backgroundColor: "#1976D2" };
   const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
 
   // if(location.pathname === '/invoice') return null
 
   return (
     <div>
       <AddClient setOpen={setOpen} open={open} />
+      <AddProduct setOpen={setOpen2} open={open2} />
       <Fab
         mainButtonStyles={mainButtonStyles}
         icon={<AddIcon />}
@@ -35,6 +39,14 @@ const FabButton = () => {
         <Action text="New Customer" onClick={() => setOpen((prev) => !prev)}>
           <PersonAddIcon />
         </Action>
+
+        <Action
+                text="New Product"
+                onClick={() => setOpen2((prev) => !prev)}
+              >
+                <AddCircleIcon />
+            </Action>
+
       </Fab>
     </div>
   );
